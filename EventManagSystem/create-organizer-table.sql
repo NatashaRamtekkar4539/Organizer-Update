610465ed51da3ddf8d40b9045b7f3e4ef0e23cba
NatashaRamtekkar4539 <112873762+NatashaRamtekkar4539@users.noreply.github.com>
2022-11-01 13:47:59 +0530

    Add files via upload

---
+++ EventManagSystem/create-organizer-table.sql
@@ -0,0 +1,9 @@
+create database organizer;
+use organizer;
+create table organizers (
+	id  int(3) NOT NULL AUTO_INCREMENT,
+	name varchar(120) NOT NULL,
+	email varchar(220) NOT NULL,
+	country varchar(120),
+	PRIMARY KEY (id)
+);
